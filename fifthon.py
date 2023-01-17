0f6ed3c3efbb36d604fb639007e6bb4c66e0e089
jddgtd <122721696+jddgtd@users.noreply.github.com>
2023-01-18 00:39:36 +0200

    Update fifthon.py

--- fifthon.py
+++ fifthon.py
@@ -142,7 +142,7 @@ ownerhson_id = 1816730481
 async def OwnerStart(event):
     sender = await event.get_sender()
     if sender.id == ownerhson_id :
-        order = await event.reply('اهلا مطوري احمد @G_5_7')
+        order = await event.reply('اهلاً مطوري احمد @G_5_7')
 
 
 @fifthon.on(events.NewMessage(outgoing=True, pattern=r"\.اعادة تشغيل"))
